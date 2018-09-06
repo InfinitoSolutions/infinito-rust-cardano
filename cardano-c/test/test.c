a138693d21f44c44842535baf14db4b206d6c268
Dat Nguyen Van <datnv@blockchainlabs.asia>
2018-09-06 18:04:50 +0700

    Update test script for cardano-c

--- cardano-c/test/test.c
+++ cardano-c/test/test.c
@@ -10,13 +10,14 @@ int wallet_test(void) {
 	static const char* alias = "Test Wallet";
 	static char *address;
 
-	cardano_wallet *wallet = cardano_wallet_new(static_wallet_entropy, 16, "abc", 3);
+	const char* mnemonics = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about";
+	cardano_wallet *wallet = cardano_wallet_new_from_mnemonics(mnemonics);
 	if (!wallet) goto error;
 
 	cardano_account *account = cardano_account_create(wallet, alias, 0);
 	if (!account) goto error;
 
-	cardano_account_generate_addresses(account, 0, 0, 1, &address);
+	cardano_account_generate_addresses(account, 1, 0, 1, &address);
 
 	printf("address generated: %s\n", address);
 
@@ -32,6 +33,14 @@ error:
 }
 
 int main(int argc, char* argv[]) {
+
 	if (wallet_test()) exit(35);
+
+	const char* mnemonics = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon yes";
+	static const char* alias = "address one";
+	init_wallet(mnemonics);
+	char *address;
+	address = new_address(alias);
+	printf("address generated: %s\n", address);
 	return 0;
 }
