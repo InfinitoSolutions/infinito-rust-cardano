2b0751d87e26636981d856e860b0d754f7d2fc15
Dat Nguyen Van <datnv@blockchainlabs.asia>
2018-09-11 14:58:25 +0700

    Update test script

--- cardano-c/test/test_ibl.c
+++ cardano-c/test/test_ibl.c
@@ -10,7 +10,7 @@ int wallet_test_ibl(void) {
     static char *address;
     static char *rootkey;
 
-    const char* mnemonics = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon";
+    const char* mnemonics = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about";
     const char* password  = "password";
 
     create_rootkey(mnemonics, password, &rootkey);
@@ -20,19 +20,26 @@ int wallet_test_ibl(void) {
 
     printf("rootkey: %s\n", rootkey);
 
-    cardano_wallet *wallet = generate_address(rootkey, alias, 0, 0, 0, 1, &address);
+    cardano_wallet *wallet = generate_address(rootkey, 0, 0, 0, 1, &address);
 
     printf("address generated: %s\n", address);
 
     printf("address is valid: %s\n", cardano_address_is_valid(address) ? "NO" : "YES");
 
-    const char *utxos = "[{\"id\": \"e28c7bf914c855a9ceaf1741a251ab3cc001bd6ccac874b02bacf2f3a40e13c4\", \"index\": 0, \"value\": 9000000}]";
-    const char *to_addrs = "[{\"addr\": \"Ae2tdPwUPEZ3to1tD3ovyREAN5AajAPWuehHRSd5kNkTqgv2zkk4W4v14cS\",\"value\": 100000}, {\"addr\": \"Ae2tdPwUPEZ9hcJjj8T6iAoLF6RgaCYJ5TRRokxZWxGYNGoTuqcAAY2xGHj\",\"value\": 200000}]";
+    const char *utxos = "[{\"id\": \"e28c7bf914c855a9ceaf1741a251ab3cc001bd6ccac874b02bacf2f3a40e13c4\", \"index\": 1, \"value\": 731962}]";
+    const char *to_addrs = "[{\"addr\": \"Ae2tdPwUPEYxuoq9NbrPB9VodNUmhdBQz9nJBk7UPisyXmPyGje3i9k3x82\",\"value\": 100000}]";
     static char *signed_trx;
     bool result = new_transaction(rootkey, utxos, address, to_addrs, &signed_trx);
-    printf("Signed trx: %s\n", signed_trx);
+    if (result) {
+        printf("Signed trx: %s\n", signed_trx);
+    } else {
+        printf("Failed to create new transaction\n");
+    }
+
     int fee = transaction_fee(rootkey, utxos, address, to_addrs);
-    printf("Trx Fee: %d\n", fee);
+    if (fee != 0) {
+        printf("Trx Fee: %d\n", fee);
+    }
 
     return 0;
 }
