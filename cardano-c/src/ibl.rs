8f888a513e6a7f08882ad15347dd69d409e69351
Unknown <congle@cardano-labo.com>
2018-09-11 17:32:23 +0700

    Create address from entropy

--- cardano-c/src/ibl.rs
+++ cardano-c/src/ibl.rs
@@ -1,11 +1,13 @@
 use cardano::address;
-use cardano::wallet::bip44;
+use cardano::wallet::{bip44, keygen};
 use cardano::util::{self, hex};
-use cardano::hdwallet::{self, XPrv};
+use cardano::hdwallet::{self, XPrv, XPRV_SIZE};
 use cardano::wallet::scheme::{Wallet};
-use cardano::bip::bip39::{self, MnemonicString, dictionary::ENGLISH};
+use cardano::bip::bip39::{self, Mnemonics, MnemonicString, dictionary};
 
-use std::{ffi, ptr};
+use cardano::bip;
+
+use std::{ffi, slice, ptr};
 use std::os::raw::{c_char};
 
 type WalletPtr  = *mut bip44::Wallet;
@@ -19,7 +21,7 @@ fn create_rootkey( mnemonics: *mut c_char
 {
     let mnemonics     = unsafe {ffi::CStr::from_ptr(mnemonics)};
     let mnemonics_str = mnemonics.to_str().unwrap();
-    let mnemonics     = MnemonicString::new(&ENGLISH, mnemonics_str.to_string()).unwrap();
+    let mnemonics     = MnemonicString::new(&dictionary::ENGLISH, mnemonics_str.to_string()).unwrap();
 
     let password      = unsafe {ffi::CStr::from_ptr(password)};
     let password_str  = password.to_str().unwrap();
@@ -34,35 +36,36 @@ fn create_rootkey( mnemonics: *mut c_char
     };
 }
 
-// fn create_rootkey2( mnemonics: *mut c_char
-//                  , password:  *mut c_char
-//                  , root_key:  *mut *mut c_char)
-// {
-//     let mnemonics     = unsafe {ffi::CStr::from_ptr(mnemonics)};
-//     let mnemonics_str = mnemonics.to_str().unwrap();
-//     let mnemonics     = MnemonicString::new(&ENGLISH, mnemonics_str.to_string()).unwrap();
+#[no_mangle]
+pub extern "C"
+fn create_rootkey_from_entropy( mnemonics: *mut c_char
+                              , password_ptr: *const u8
+                              , password_size: usize
+                              , root_key:  *mut *mut c_char)
+{
+    let password = unsafe {slice::from_raw_parts(password_ptr, password_size)};
+    
+    let mnemonics = unsafe {ffi::CStr::from_ptr(mnemonics)};
+    let mnemonics_str = mnemonics.to_str().unwrap();
+    let mnemonics = match Mnemonics::from_string(&dictionary::ENGLISH, mnemonics_str) {
+        Err(_) => return,
+        Ok(e) => e,
+    };
 
-//     let password      = unsafe {ffi::CStr::from_ptr(password)};
-//     let password_str  = password.to_str().unwrap();
-//     let password      = password_str.as_bytes();
+    let entropy = match bip::bip39::Entropy::from_mnemonics(&mnemonics) {
+        Err(_) => return,
+        Ok(e) => e,
+    };
 
-//     let entropy = match Entropy::from_mnemonics(&mnemonics) { 
-//         Err(_) => return ptr::null_mut(), 
-//         Ok(e) => e, 
-//     };
-//     let mut seed = [0u8; XPRV_SIZE];
-//     keygen::generate_seed(&entropy, &password, &mut seed);
-//     let xprv = XPrv::normalize_bytes(seed);
+    let mut seed = [0u8; XPRV_SIZE];
+    keygen::generate_seed(&entropy, password, &mut seed);
+    let xprv = XPrv::normalize_bytes(seed);
 
-
-//     // let seed = bip39::Seed::from_mnemonic_string(&mnemonics, &password);
-//     // let xprv = XPrv::generate_from_bip39(&seed);
-
-//     let xprv_ptr = ffi::CString::new(xprv.to_string()).expect("base58 strings only contains ASCII chars");
-//     unsafe {
-//         ptr::write(root_key, xprv_ptr.into_raw())
-//     };
-// }
+    let xprv_ptr = ffi::CString::new(xprv.to_string()).expect("base58 strings only contains ASCII chars");
+    unsafe {
+        ptr::write(root_key, xprv_ptr.into_raw())
+    };
+}
 
 #[no_mangle]
 pub extern "C"
@@ -93,28 +96,6 @@ fn delete_wallet(wallet_ptr: WalletPtr)
     };
 }
 
-// #[no_mangle]
-// pub extern "C"
-// fn create_account( root_key: *mut c_char
-//                  , account_alias: *mut c_char
-//                  , account_index: u32)
-//     -> AccountPtr
-// {
-//     let wallet_ptr = create_wallet(root_key);
-//     let wallet     = unsafe {wallet_ptr.as_mut()}.expect("Not a NULL PTR");
-
-//     let account_alias = unsafe {
-//         ffi::CStr::from_ptr(account_alias).to_string_lossy()
-//     };
-
-//     let account     = wallet.create_account(&account_alias, account_index);
-//     let account_box = Box::new(account.public()); 
-
-//     delete_wallet(wallet_ptr);
-
-//     Box::into_raw(account_box)
-// }
-
 #[no_mangle]
 pub extern "C"
 fn delete_account(account_ptr: AccountPtr)
@@ -127,7 +108,6 @@ fn delete_account(account_ptr: AccountPtr)
 #[no_mangle]
 pub extern "C"
 fn generate_address( root_key: *mut c_char
-                   , account_alias: *mut c_char
                    , account_index: u32
                    , internal: bool
                    , from_index: u32
@@ -137,15 +117,8 @@ fn generate_address( root_key: *mut c_char
 {
     let wallet_ptr = create_wallet(root_key);
     let wallet     = unsafe {wallet_ptr.as_mut()}.expect("Not a NULL PTR");
-    // let account_ptr = create_account(root_key, account_alias, account_index);
-    // let account     = unsafe {account_ptr.as_mut()}.expect("Not a NULL PTR");
 
-    let account_alias = unsafe {
-        ffi::CStr::from_ptr(account_alias).to_string_lossy()
-    };
-
-
-    let account     = wallet.create_account(&account_alias, account_index);
+    let account     = wallet.create_account("", account_index);
     let account_box = Box::new(account.public()); 
     let account_ptr = Box::into_raw(account_box);
     let account     = unsafe {account_ptr.as_mut()}.expect("Not a NULL PTR");
--- cardano-c/test/test_ibl.c
+++ cardano-c/test/test_ibl.c
@@ -5,21 +5,20 @@
 #include "ibl.h"
 
 int wallet_test_ibl(void) {
-    static const char* alias = "Test Wallet";
     static char *address;
     static char *rootkey;
 
-    const char* mnemonics = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon";
+    const char* mnemonics = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about";
     const char* password  = "password";
 
-    create_rootkey(mnemonics, password, &rootkey);
+    create_rootkey_from_entropy(mnemonics, password, strlen(password), &rootkey);
     if (!rootkey) {
         return -1;
     }
 
     printf("rootkey: %s\n", rootkey);
 
-    cardano_wallet *wallet = generate_address(rootkey, alias, 0, 0, 0, 1, &address);
+    cardano_wallet *wallet = generate_address(rootkey, 0, 0, 0, 1, &address);
 
     printf("address generated: %s\n", address);
 
